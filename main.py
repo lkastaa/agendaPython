d689638a34f99b017cc7f555e04ede91555cf05e
Luiz Karpinski <euluizkarpinski@gmail.com>
2025-06-30 00:08:51 -0300

    feat(contacts): toggle favorite function

--- main.py
+++ main.py
@@ -10,6 +10,23 @@ def listContacts():
       favorite = "★" if contact["favorite"] else "✩"
       print(f"{favorite} {i} - {contact["name"]} ")
 
+def createContact():
+  name = input("Digite o nome do contato: ")
+  phone = int(input(f"Digite o telefone para o novo contato {name}: "))
+  mail = input(f"Digite um email para o novo contato {name}: ")
+  favorite = False
+
+  contacts.append({
+    "name":name,
+    "phone": phone,
+    "maill": mail,
+    "favorite":favorite
+  })
+
+def toggleFavorite(index):
+  correctIndex = index -1
+  contacts[correctIndex]["favorite"] = not contacts[correctIndex]["favorite"]
+
 while True:
   print("\nLista de Contatos")
   print("1. Mostrar Contatos")
@@ -19,10 +36,18 @@ while True:
   print("5. Excluir Contato")
   print("6. Fechar Lista")
 
-  chose = int(input("\nSelecione uma opção para continuar:"))
+  chose = int(input("\nSelecione uma opção para continuar: "))
 
   if(chose == 6):
     break
   elif(chose == 1):
     listContacts()
-
+  elif(chose == 2):
+    print("\nAdicionar novo contato.")
+    createContact()
+  elif(chose == 3):
+    toggleFavoriteIndex = int(input("\nQual contato voce deseja favoritar ou desfavoritar: "))
+    listContacts()
+    toggleFavorite(toggleFavoriteIndex)
+    print('\nLista de Contatos Atualizada: ')
+    listContacts()
