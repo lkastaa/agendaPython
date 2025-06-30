db8e2c6dc3560c6f30541ecd2be5ee0b7c5b9cc1
Luiz Karpinski <euluizkarpinski@gmail.com>
2025-06-30 00:19:20 -0300

    feat(contacts): create a eddit contact function

--- main.py
+++ main.py
@@ -27,6 +27,19 @@ def toggleFavorite(index):
   correctIndex = index -1
   contacts[correctIndex]["favorite"] = not contacts[correctIndex]["favorite"]
 
+def edditContact(index):
+  correctIndex = index -1
+  newName = input(f"Digite o novo nome para {contacts[correctIndex]["name"]}: ")
+  newPhone = int(input(f"Digite o novo numero para {newName}: "))
+  newMail = input("Digite o novo endereco de email: ")
+
+  contacts[correctIndex] = {
+    "name": newName,
+    "phone": newPhone,
+    "mail": newMail,
+    "favorite": contacts[correctIndex]["favorite"]
+  }
+
 while True:
   print("\nLista de Contatos")
   print("1. Mostrar Contatos")
@@ -40,14 +53,20 @@ while True:
 
   if(chose == 6):
     break
-  elif(chose == 1):
+  elif(chose == 1): #Listar Contatos
     listContacts()
-  elif(chose == 2):
+  elif(chose == 2): #Adicionar Contato
     print("\nAdicionar novo contato.")
     createContact()
-  elif(chose == 3):
+  elif(chose == 3): #Favoritar Contato
     toggleFavoriteIndex = int(input("\nQual contato voce deseja favoritar ou desfavoritar: "))
     listContacts()
     toggleFavorite(toggleFavoriteIndex)
     print('\nLista de Contatos Atualizada: ')
     listContacts()
+  elif(chose == 4):
+    toggleEdditIndex = int(input("\nQual contato voce deseja editar: "))
+    listContacts()
+    edditContact(toggleEdditIndex)
+    listContacts()
+
